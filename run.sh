155507a78f066e80dfefcd1242f368bba9e935ed
4thhia <4thhia@gmail.com>
2025-02-10 13:38:28 +0900

    -

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-USER="4thhia"
-
-docker container run --rm -it \
-    -p 4000:4000 \
-    -u $(id -u):$(id -g) \
-    -v $(pwd):/srv/jekyll \
-    settai_jekyll
--- setup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-bundle
-bundle exec jekyll serve --host 0.0.0.0
